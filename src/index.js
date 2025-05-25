8da60e3e673b66b41e61aec1ca1f71d9b8cdc780
kharchenko7002 <kharchenko7002@gmail.com>
2025-05-25 13:53:46 +0200

    design

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 // src/index.js
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.js"; // Убедись, что файл App.js существует и экспортирует компонент App
+import App from "./App.js"; 
 import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
